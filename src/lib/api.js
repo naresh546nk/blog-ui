42951e973007e5a4345c1e13b6decfc14c489e06
naresh546nk <91820009+naresh546nk@users.noreply.github.com>
2023-10-29 17:17:53 +0530

    Update api.js

--- src/lib/api.js
+++ src/lib/api.js
@@ -5,7 +5,7 @@ logger.log("process.env:", process.env);
 var BASEURL = "http://localhost:9000";
 if (process.env?.NODE_ENV === "production") {
   BASEURL =
-    "http://ecs-LoadBala-kdlHFtsN7FMs-87d8a5429e620070.elb.ap-south-1.amazonaws.com:9000";
+    "http://ecs-LoadBala-aNc8nTIegD6M-9b1fa76ed417bb4e.elb.ap-south-1.amazonaws.com:9000";
 }
 const DOMAIN_URL = BASEURL + "/api/v1.0/blogsite";
 logger.log("BASEURL :", BASEURL);
