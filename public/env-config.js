c407a4789b8a807234527df3d1b5ee508c99882b
naresh546nk <91820009+naresh546nk@users.noreply.github.com>
2023-11-04 09:58:57 +0530

    working on env variable

--- public/env-config.js
+++ public/env-config.js
@@ -1,4 +1,6 @@
 window._env_ = {
   REACT_APP_API_BASE_URL: "https://example.com",
   REACT_APP_TOKEN: " randomTokenNumber",
+  TOKEN: "TOKEN VARIABLE IS ADDED AS PART OF TESTING",
+  TOKEN1: "TOKEN VARIABLE IS ADDED AS PART OF TESTING_1",
 }
